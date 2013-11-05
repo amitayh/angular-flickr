dca3fcde68f89678a2698f848aadbe79b2e2ac32
Amitay Horwitz <amitayh@gmail.com>
2013-11-05 14:57:04 +0200

    Flickr spec

--- test/FlickrSpec.js
+++ test/FlickrSpec.js
@@ -18,31 +18,30 @@ describe('services.flickr', function () {
     });
 
     it('should reject promise on invalid response', function() {
-        var expected = 'Something went wrong',
-            data = {message: expected},
-            actual;
+        var message = 'Something went wrong',
+            data = {message: message},
+            success = jasmine.createSpy(),
+            error = jasmine.createSpy();
 
         httpBackend.expectJSONP(/api\.flickr\.com/).respond(data);
-        flickr.getSets('1234').then(
-            function() {},
-            function(error) { actual = error; }
-        );
+        flickr.getSets('1234').then(success, error);
         httpBackend.flush();
 
-        expect(actual).toEqual(expected);
+        expect(success).not.toHaveBeenCalled();
+        expect(error).toHaveBeenCalledWith(message);
     });
 
     describe("'getSets' method", function() {
         it('should fetch sets for user ID', function() {
             var photosets = ['photoset1', 'photoset2'],
                 url = gateway + '?method=flickr.photosets.getList&user_id=1234&api_key=fake&format=json&jsoncallback=JSON_CALLBACK',
-                data = {stat: 'ok', photosets: photosets};
+                data = {stat: 'ok', photosets: photosets},
+                callback = jasmine.createSpy();
 
             httpBackend.expectJSONP(url).respond(data);
-            flickr.getSets('1234').then(function(response) {
-                expect(response).toEqual(photosets);
-            });
+            flickr.getSets('1234').then(callback);
             httpBackend.flush();
+            expect(callback).toHaveBeenCalledWith(photosets);
         });
         it('should fetch sets page', function() {
             var url = gateway + '?method=flickr.photosets.getList&user_id=1234&per_page=10&page=5&api_key=fake&format=json&jsoncallback=JSON_CALLBACK',
@@ -58,13 +57,14 @@ describe('services.flickr', function () {
         it('should fetch set info', function() {
             var photoset = {id: 1234, name: 'foo'},
                 url = gateway + '?method=flickr.photosets.getInfo&photoset_id=1234&api_key=fake&format=json&jsoncallback=JSON_CALLBACK',
-                data = {stat: 'ok', photoset: photoset};
+                data = {stat: 'ok', photoset: photoset},
+                callback = jasmine.createSpy();
 
             httpBackend.expectJSONP(url).respond(data);
-            flickr.getSetInfo('1234').then(function(response) {
-                expect(response).toEqual(photoset);
-            });
+            flickr.getSetInfo('1234').then(callback);
             httpBackend.flush();
+
+            expect(callback).toHaveBeenCalledWith(photoset);
         });
     });
 
@@ -72,13 +72,14 @@ describe('services.flickr', function () {
         it('should fetch photos in photo set', function() {
             var photoset = {id: 1234, name: 'foo'},
                 url = gateway + '?method=flickr.photosets.getPhotos&photoset_id=1234&api_key=fake&format=json&jsoncallback=JSON_CALLBACK',
-                data = {stat: 'ok', photoset: photoset};
+                data = {stat: 'ok', photoset: photoset},
+                callback = jasmine.createSpy();
 
             httpBackend.expectJSONP(url).respond(data);
-            flickr.getPhotos('1234').then(function(response) {
-                expect(response).toEqual(photoset);
-            });
+            flickr.getPhotos('1234').then(callback);
             httpBackend.flush();
+
+            expect(callback).toHaveBeenCalledWith(photoset);
         });
         it('should fetch photos page in photo set', function() {
             var url = gateway + '?method=flickr.photosets.getPhotos&photoset_id=1234&per_page=10&page=5&api_key=fake&format=json&jsoncallback=JSON_CALLBACK',
@@ -94,7 +95,8 @@ describe('services.flickr', function () {
         it('should return a valid Flickr photo URL', function() {
             var photo = {farm: '1', server: 2, id: 3, secret: 4},
                 expected = 'http://farm1.static.flickr.com/2/3_4_s.jpg';
-            expect(service.getPhotoUrl(photo, service.Size.Square)).toBe(expected);
+
+            expect(service.getPhotoUrl(photo, service.Size.Square)).toEqual(expected);
         });
     });
 
